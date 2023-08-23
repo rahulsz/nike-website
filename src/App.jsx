cfaea98e946fdc1a5d8ac3e523f3921d48e4f05a
Rahul <rahulzandennavar99@gmail.com>
2023-08-23 17:01:39 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,13 @@ import{
   Footer
 } from "./sections"
 
+import  Nav from "./components/Nav";
+
 
 const App = () => {
   return (
     <main className="relative">
-      {/*<Nav/>*/}
+      <Nav/>
       <section className="xl:padding-1 wide:padding-r padding-b">  
       <Hero/></section>
       <section className="padding">
