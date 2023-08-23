558cafbb166f5125fb9ab580a5f1e8052c9b58b5
Rahul <rahulzandennavar99@gmail.com>
2023-08-23 16:31:34 +0530

    Creating Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,42 @@
 
+import{
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  CustomerReviews,
+  Subscribe,
+  Footer
+} from "./sections"
+
 
 const App = () => {
   return (
     <main className="relative">
-      Nav
+      {/*<Nav/>*/}
       <section className="xl:padding-1 wide:padding-r padding-b">  
-      Hero</section>
+      <Hero/></section>
       <section className="padding">
-        PopularProducts
+        <PopularProducts/>
       </section>
       <section className="padding">
-       SuperQuality
+       <SuperQuality/>
       </section>
       <section className="padding">
-        Services
+        <Services/>
       </section>
       <section className="padding">
-       SpecialOffer
+       <SpecialOffers/>
       </section>
       <section className="bg-pale-blue padding">
-       CustomerReviews
+       <CustomerReviews/>
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-       Subscribe
+       <Subscribe/>
       </section>
       <section className="bg-black padding-x padding-t pb-8">
-       Footer
+       <Footer/>
       </section>
 
     </main >
